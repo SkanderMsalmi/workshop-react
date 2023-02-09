49c38ee722a7ee36f3d3c03b66f64307a0b2b892
Skander <msalmi.skander@esprit.tn>
2023-02-09 12:14:17 +0100

    Atelier 1 Done
    - Alert Buy Item
    - Alert Info open Page

---
+++ src/Components/Product.js
@@ -0,0 +1,56 @@
+import { Component } from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { Alert } from "react-bootstrap";
+
+export default class Product extends Component{
+
+    constructor(props){
+        super(props);
+        this.state =  {like : this.props.product.like,quantity:this.props.product.quantity, alertVisible:false};
+        this.addLikes = this.addLikes.bind(this);
+        this.buyProducts = this.buyProducts.bind(this);
+        this.handleVisible = this.handleVisible.bind(this);
+    }
+
+    addLikes(){
+        this.setState((oldState)=>({
+            like : oldState.like +1
+        }));
+    }
+
+    buyProducts (){
+        this.setState((oldState)=>({
+            quantity:oldState.quantity-1
+        }));
+        this.handleVisible()
+    }   
+
+    handleVisible = () => { 
+        this.setState({alertVisible:true})
+        setTimeout(() => { 
+            this.setState({alertVisible:false})
+        }, 2000);
+    } 
+
+    render(){
+        return (<>
+            <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={require('../assets/images/'+this.props?.product.img)} />
+            <Card.Body>
+              <Card.Title>{this.props.product.name}</Card.Title>
+              <Card.Text>
+                {this.props.product.description}
+              </Card.Text>
+              <Card.Text>
+                {this.props.product.price} DT
+              </Card.Text>
+              <Button variant="primary" onClick={this.addLikes} >Like</Button> <small>{this.state.like} Likes</small>
+              <Button variant="primary" onClick={this.buyProducts} disabled={this.state.quantity === 0}>Buy</Button> <small>{this.state.quantity} Products</small>
+            </Card.Body>
+            <Alert variant="primary" show={this.state?.alertVisible}  >you bought an Item !!!!</Alert>    
+          </Card>
+          </>
+        );
+    }
+}
---
+++ src/Components/Products.js
@@ -0,0 +1,37 @@
+import { Component } from "react";
+import Product from "./Product";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Alert } from "react-bootstrap";
+import ProductsList from '../products.json';
+export default class Products extends Component{
+    constructor(props) {
+        super(props);
+        this.state ={alertVisible:false}
+        this.handleVisible = this.handleVisible.bind(this);
+    }
+    handleVisible = () => { 
+        this.setState({alertVisible:true})
+        setTimeout(() => { 
+            this.setState({alertVisible:false})
+        }, 3000);
+    } 
+    componentDidMount() {
+    this.handleVisible();
+    }
+
+
+    render(){
+        return (
+            <div>
+                <Alert variant="success" show={this.state?.alertVisible}><Alert.Heading>Hey, Welcome To Our Shop <strong>MyStore</strong></Alert.Heading>
+                <p>Thank you for choosing our store, we hope you enjoy your shopping experience!</p></Alert>
+                <h1>Products</h1>
+                <Row>
+        {ProductsList.map((prod,i)=>(<Col key={i}><Product  product={prod}></Product></Col>))}
+      </Row>
+            </div>
+     
+        );
+    }
+}
