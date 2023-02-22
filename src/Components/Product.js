97d9e3cfd22d90b4c61bd35c1dbbefa13f264a86
Skander <msalmi.skander@esprit.tn>
2023-02-23 00:28:35 +0100

    bestProduct & fonctionnel

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,58 +1,76 @@
-import { Component } from "react";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import { Alert } from "react-bootstrap";
+import {  useState } from "react";
+import { Alert } from 'react-bootstrap';
+import styles from './Product.module.css';
+// const add = addLikes() =>{
+//     this.setState((oldState)=>({
+//         like : oldState.like +1
+//     }));
+// }
+// buyProducts (){
+//     this.setState((oldState)=>({
+//         quantity:oldState.quantity-1
+//     }));
+//     this.handleVisible()
+// }
+// handleVisible = () => { 
+//     this.setState({alertVisible:true})
+//     setTimeout(() => { 
+//         this.setState({alertVisible:false})
+//     }, 2000);
+// }    
+  function Product ({product}){
+    const [like, addLikes] = useState(product.like);
+    const [quantity, addQuantity] = useState(product.quantity);
+    const [alertVisible,toggleAlertVisible] =useState(false);
+    const [bestProduct,setBestProduct]= useState(false);
 
-export default class Product extends Component{
+    let bestProductBackground = bestProduct ? styles.bestProduct : null;
+    const toggleBestProduct = ()=> {
+        if (like >= 4){
+          setBestProduct(true);
+        }
+      }
 
-    constructor(props){
-        super(props);
-        this.state =  {like : this.props.product.like,quantity:this.props.product.quantity, alertVisible:false};
-        this.addLikes = this.addLikes.bind(this);
-        this.buyProducts = this.buyProducts.bind(this);
-        this.handleVisible = this.handleVisible.bind(this);
+    const setAlertVisible = ()=>{
+      toggleAlertVisible(true);
+      setTimeout(() => {
+        toggleAlertVisible(false);
+      }, 2000);
     }
 
-    addLikes(){
-        this.setState((oldState)=>({
-            like : oldState.like +1
-        }));
+    const setLikes = ()=>{
+        addLikes(like+1);
+        toggleBestProduct();
     }
 
-    buyProducts (){
-        this.setState((oldState)=>({
-            quantity:oldState.quantity-1
-        }));
-        this.handleVisible()
-    }   
+    const setQuantity = ()=>{
+        addQuantity(quantity-1);
+        setAlertVisible();
+    }
 
-    handleVisible = () => { 
-        this.setState({alertVisible:true})
-        setTimeout(() => { 
-            this.setState({alertVisible:false})
-        }, 2000);
-    } 
-
-    render(){
-        return (<>
-            <Card style={{ width: '25rem' , minHeight:'650px',display:'flex',padding:'20px'}}>
-            <Card.Img variant="top" src={require('../assets/images/'+this.props?.product.img)} />
+        return (
+        <>
+            <Card style={{ width: '25rem' , minHeight:'650px',display:'flex',padding:'20px'}} className={bestProductBackground}>
+            <Card.Img variant="top" src={require('../assets/images/'+product.img)} />
             <Card.Body>
-              <Card.Title>{this.props.product.name}</Card.Title>
+              <Card.Title>{product.name}</Card.Title>
               <Card.Text>
-                {this.props.product.description}
+                {product.description}
               </Card.Text>
               <Card.Text>
-                {this.props.product.price} DT
+                {product.price} DT
               </Card.Text>
               <div style={{display:'flex',justifyContent:'center',alignItems:'center'}}>
-              <Button variant="primary" onClick={this.addLikes} style={{margin :'15px'}} >Like</Button> <small style={{marginRight:'25px'}}>{this.state.like} Likes</small>
-              <Button variant="primary" onClick={this.buyProducts} disabled={this.state.quantity === 0} style={{margin :'15px'}}>Buy</Button> <small>{this.state.quantity} Products</small>
+              <Button variant="primary" style={{margin :'15px'}} onClick={setLikes} >Like</Button> <small style={{marginRight:'25px'}}> {like} Likes</small>
+              <Button variant="danger"  style={{margin :'15px'}} onClick={setQuantity} disabled={quantity<1}>Buy</Button> <small> {quantity} Products</small>
               </div>
             </Card.Body>
-            <Alert variant="primary" show={this.state?.alertVisible}  >you bought an Item !!!!</Alert>    
+            <Alert variant="primary" show={alertVisible}  >you bought an Item !!!!</Alert>    
           </Card>
           </>
         );
-    }
 }
+
+export default Product;
--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,30 +1,40 @@
-import { Component } from "react";
 import Product from "./Product";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { Alert } from "react-bootstrap";
 import ProductsList from '../products.json';
-export default class Products extends Component{
-    constructor(props) {
-        super(props);
-        this.state ={alertVisible:false}
-        this.handleVisible = this.handleVisible.bind(this);
-    }
-    handleVisible = () => { 
-        this.setState({alertVisible:true})
-        setTimeout(() => { 
-            this.setState({alertVisible:false})
+import { useEffect, useState } from "react";
+
+// constructor(props) {
+//     super(props);
+//     this.state ={alertVisible:false}
+//     this.handleVisible = this.handleVisible.bind(this);
+// }
+
+// handleVisible = () => { 
+//     this.setState({alertVisible:true})
+//     setTimeout(() => { 
+//         this.setState({alertVisible:false})
+//     }, 3000);
+// } 
+// componentDidMount() {
+// this.handleVisible();
+// }
+
+
+
+function Products(){
+    
+    const [alertVisible,toggleAlertVisible] = useState(true);
+    useEffect(()=>{
+        setInterval(() => {
+            toggleAlertVisible(false);
         }, 3000);
-    } 
-    componentDidMount() {
-    this.handleVisible();
-    }
+    })
 
-
-    render(){
         return (
             <div>
-                <Alert variant="success" show={this.state?.alertVisible}><Alert.Heading>Hey, Welcome To Our Shop <strong>MyStore</strong></Alert.Heading>
+                <Alert variant="success" show={alertVisible} ><Alert.Heading>Hey, Welcome To Our Shop <strong>MyStore</strong></Alert.Heading>
                 <p>Thank you for choosing our store, we hope you enjoy your shopping experience!</p></Alert>
                 <h1 style={{textAlign:'center',margin:'50px'}}>Products</h1>
                 <Row>
@@ -33,5 +43,6 @@ export default class Products extends Component{
             </div>
      
         );
-    }
 }
+
+export default Products;
